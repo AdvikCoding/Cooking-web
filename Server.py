dd811fa54b554735cd758957cdbef7007c53e293
AdvikCoding <adchengta@gmail.com>
2025-12-12 15:25:57 +0000

    Banana Cake page done with EVERYTHING including original recipie link at the bottom.

--- Server.py
+++ Server.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')  # Flask looks in 'templates' folder by default
+@app.route('/home')
+def new_home():
+    return render_template('index.html')  # Flask looks in 'templates' folder by default
 @app.route('/banana')
 def banana():
     return render_template('banana_bread.html')  # Flask looks in 'templates' folder by default
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 
