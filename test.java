d209015ec3c446c4d97df114f5245ac5102a9a11
AdvikCoding <adchengta@gmail.com>
2025-12-19 04:30:40 +0000

    Recipies webpage done with new design! Very stylish, figuring out how to implement a search function, although I am thinking of just using browser bulit in find stuff on page but also want to add a search button.

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.nert com.sun.net.httpserver.*;
-public class test{
-    public static void main(String args[]) throws Exception {
-        System.err.println("hi");
-        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0",8000), 0);
-        server.createContext("/", new HttpHandler() {
-            public void handle(HttpExchange exchange) throws IOE
-}
